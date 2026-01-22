6fbbce7cb2214d342d77286ef873d671b5af44d7
Anand Raj <anadraj73073@gmail.com>
2026-01-23 03:16:14 +0530

    Build separate folder for socketServer and nextjs

---
+++ nextfolder/src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export { default } from "next-auth/middleware";
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/sign-in", "/sign-up", "/", "/verify/:path*"],
+};
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  // Redirect authenticated users away from sign-in/sign-up
+  if (
+    token &&
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/verify"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  // Redirect unauthenticated users away from the dashboard
+  if (!token && url.pathname.startsWith("/dashboard")) {
+    console.log("token2", token);
+
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
